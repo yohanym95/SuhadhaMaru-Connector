50c18d1055cffb6550668b0536905e9499b0fa05
yohanym95 <malshikay@gmail.com>
2020-04-14 12:17:45 +0530

    Get notification through channel

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ admin.initializeApp(functions.config().firebase);
 //  response.send("Hello from Firebase!");
 // });
 
-exports.helloWorld = functions.database.ref('Posts/{pushId}').onWrite((change,context) => {
+exports.pushPostUpdate = functions.database.ref('Posts/{pushId}').onWrite((change,context) => {
     const post = change.after.val();
 
     const payload = {
@@ -25,7 +25,37 @@ exports.helloWorld = functions.database.ref('Posts/{pushId}').onWrite((change,co
         if (allToken.val()) {
             console.log('token available');
             const token = Object.keys(allToken.val());
-            return admin.messaging().sendToDevice(token, payload)
+            return admin.messaging().sendToTopic("Post", payload)
+                .then(function (response) {
+                    console.log('Notification sent successfully:', response);
+                })
+                .catch(function (error) {
+                    console.log('Notification sent failed:', error);
+                });
+        } else {
+            console.log('token not available');
+        }
+    })
+});
+
+
+exports.pushAppUpdate = functions.database.ref('Notification/AppUpdate/{Id}').onWrite((change,context) => {
+    //const post = change.after.val();
+
+    const payload = {
+        notification: {
+            title: `New Update`,
+            body: `New Update`,
+            sound: "default"
+        }
+    }
+
+    return admin.database().ref('fcm-token').once('value').then(allToken => {
+
+        if (allToken.val()) {
+            console.log('token available');
+            const token = Object.keys(allToken.val());
+            return admin.messaging().sendToTopic("AppUpdate", payload)
                 .then(function (response) {
                     console.log('Notification sent successfully:', response);
                 })
